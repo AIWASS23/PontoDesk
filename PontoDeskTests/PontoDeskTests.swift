abd83659c6e7a5ce48cd60783e8f5ac51204f36b
AIWASS23 <marcelodearaujo677@gmail.com>
2024-07-29 08:59:54 -0300

    init/ iniciando o projeto

--- PontoDeskTests/PontoDeskTests.swift
+++ PontoDeskTests/PontoDeskTests.swift
@@ -5,11 +5,10 @@
 //  Created by marcelodearaujo on 29/07/24.
 //
 
-import XCTest
 @testable import PontoDesk
+import XCTest
 
 final class PontoDeskTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -28,9 +27,8 @@ final class PontoDeskTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
--- PontoDeskUITests/PontoDeskUITestsLaunchTests.swift
+++ PontoDeskUITests/PontoDeskUITestsLaunchTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 final class PontoDeskUITestsLaunchTests: XCTestCase {
-
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
