8db817e65fc14f190124a3cb2a9c698ecc27baaf
AIWASS23 <marcelodearaujo677@gmail.com>
2024-07-29 08:43:46 -0300

    Initial Commit

---
+++ PontoDesk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PontoDesk
+//
+//  Created by marcelodearaujo on 29/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PontoDesk/PontoDeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  PontoDeskApp.swift
+//  PontoDesk
+//
+//  Created by marcelodearaujo on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct PontoDeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
