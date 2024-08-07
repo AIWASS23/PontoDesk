f852e90fd56304d08c4f7d1ef8fa41f2d334d09d
Yasmin-Carloto <yasmincarlotobezerra17@gmail.com>
2024-08-07 10:35:48 -0300

    feat: adding tap gesture to login button's style

--- PontoDesk/Utils/Styles/ButtonLoginStyle.swift
+++ PontoDesk/Utils/Styles/ButtonLoginStyle.swift
@@ -9,14 +9,18 @@ import SwiftUI
 
 struct ButtonLoginStyle: ButtonStyle {
     var parentContainerSize: GeometryProxy
+    @State var buttonClicked: Bool = true
     
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .frame(width: parentContainerSize.size.width/3, height: parentContainerSize.size.height/24)
             .padding(6)
-            .background(.bgDarkBlue)
+            .background(buttonClicked ? .bgDarkBlue : .bgBlue)
             .cornerRadius(10)
             .padding(parentContainerSize.size.width/80)
             .foregroundColor(.white)
+            .onTapGesture {
+                buttonClicked.toggle()
+            }
     }
 }
