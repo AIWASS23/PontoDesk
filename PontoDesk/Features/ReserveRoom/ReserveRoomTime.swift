6516d7d9948e79173176fa4b158433c3cc08e366
AIWASS23 <marcelodearaujo677@gmail.com>
2024-08-08 11:48:54 -0300

    refactor/ UI não habilitada no MacOS

--- PontoDesk/Features/ReserveRoom/ReserveRoomTime.swift
+++ PontoDesk/Features/ReserveRoom/ReserveRoomTime.swift
@@ -5,15 +5,16 @@
 //  Created by marcelodearaujo on 08/08/24.
 //
 
-
+//
 //import SwiftUI
 //
 //struct RoomReservationView: View {
 //    @State private var name: String = ""
-//    @State private var entryTimeIndex: Int = 0
-//    @State private var exitTimeIndex: Int = 0
+//    @State private var entryTimeIndex: Date = .now
+//    @State private var exitTimeIndex = Date()
 //    @State var selectedDate: Date = .now
-//    
+//    @State private var alarm = Date()
+//
 //    let times: [String] = {
 //        var timesArray = [String]()
 //        let formatter = DateFormatter()
@@ -26,45 +27,75 @@
 //        }
 //        return timesArray
 //    }()
-//    
+//
 //    var body: some View {
 //        VStack(spacing: 20) {
 //            Text("Reserva de Sala")
 //                .font(.largeTitle)
 //                .padding(.top, 20)
-//            
+//
 //            TextField("Quem vai usar a sala?", text: $name)
 //                .textFieldStyle(.roundedBorder)
 //                .padding(.horizontal)
 //                .overlay(
 //                    RoundedRectangle(cornerRadius: 7)
-//                        .stroke(.blue, lineWidth: 2)
+//                        .stroke(.blue, lineWidth: 1)
 //                        .padding(.horizontal)
 //                )
-//                
-//            
-//            Picker("Horário de Entrada", selection: $entryTimeIndex) {
-//                ForEach(0..<times.count, id: \.self) {
-//                    Text(self.times[$0])
-//                }
-//            }
-//            .pickerStyle(.menu)
-//            .labelsHidden()
+//
+//            //            Picker("Horário de Entrada", selection: $entryTimeIndex) {
+//            //                ForEach(0..<times.count, id: \.self) {
+//            //                    Text(self.times[$0])
+//            //                }
+//            //            }
+//            //            .padding(.horizontal)
+//            //            .onChange(of: entryTimeIndex) { _, newValue in
+//            //                adjustEntryTime(newValue)
+//            //            }
+//            //            .overlay(
+//            //                RoundedRectangle(cornerRadius: 7)
+//            //                    .stroke(.blue, lineWidth: 1)
+//            //                    .padding(.horizontal)
+//            //            )
+//            //
+//            //            Picker("Horário de Saída", selection: $exitTimeIndex) {
+//            //                ForEach(0..<times.count, id: \.self) {
+//            //                    Text(self.times[$0])
+//            //                }
+//            //            }
+//            //            .padding(.horizontal)
+//            //            .onChange(of: exitTimeIndex) { _, newValue in
+//            //                adjustExitTime(newValue)
+//            //            }
+//            //            .overlay(
+//            //                RoundedRectangle(cornerRadius: 7)
+//            //                    .stroke(.blue, lineWidth: 1)
+//            //                    .padding(.horizontal)
+//            //            )
+//
+//
+//
+//            DatePicker("Horário de Entrada",
+//                       selection: $entryTimeIndex,
+//                       displayedComponents: .hourAndMinute
+//            )
 //            .padding(.horizontal)
-//            
-//            Picker("Horário de Saída", selection: $exitTimeIndex) {
-//                ForEach(0..<times.count, id: \.self) {
-//                    Text(self.times[$0])
-//                }
-//            }
-//            .pickerStyle(.menu)
-//            .labelsHidden()
+//            .hLeading()
+//            .datePickerStyle(.stepperField)
+//
+//            DatePicker("Horário de Saída",
+//                       selection: $exitTimeIndex,
+//                       displayedComponents: .hourAndMinute
+//            )
 //            .padding(.horizontal)
-//                                   
+//            .hLeading()
+//            .datePickerStyle(.stepperField)
+//
+//
 //            Spacer()
-//            
+//
 //            Button(action: {
-//                reserveRoom()
+//                //reserveRoom()
 //            }) {
 //                Text("Reservar Sala")
 //                    .foregroundColor(.white)
@@ -75,40 +106,36 @@
 //            .padding(.bottom, 20)
 //        }
 //    }
-//    
-//    func reserveRoom() {
-//        let entryTimeString = times[entryTimeIndex]
-//        let exitTimeString = times[exitTimeIndex]
-//        print("Sala reservada por \(name) das \(entryTimeString) às \(exitTimeString)")
-//    }
+////
+////    func adjustEntryTime(_ newValue: Int) {
+////        if newValue >= exitTimeIndex {
+////            entryTimeIndex = exitTimeIndex - 1
+////        }
+////    }
+////
+////    func adjustExitTime(_ newValue: Int) {
+////        if newValue <= entryTimeIndex {
+////            exitTimeIndex = entryTimeIndex + 1
+////        }
+////    }
+////
+////    func reserveRoom() {
+////        let entryTimeString = times[entryTimeIndex]
+////        let exitTimeString = times[exitTimeIndex]
+////        print("Sala reservada por \(name) das \(entryTimeString) às \(exitTimeString)")
+////    }
 //}
 //
 //#Preview {
 //    RoomReservationView()
 //}
-//
-//
 
 import SwiftUI
 
 struct RoomReservationView: View {
     @State private var name: String = ""
-    @State private var entryTimeIndex: Int = 0
-    @State private var exitTimeIndex: Int = 1 // Inicialmente maior que entryTimeIndex
-    @State var selectedDate: Date = .now
-    
-    let times: [String] = {
-        var timesArray = [String]()
-        let formatter = DateFormatter()
-        formatter.dateFormat = "HH:mm"
-        var date = Calendar.current.date(bySettingHour: 8, minute: 0, second: 0, of: Date())!
-        let endDate = Calendar.current.date(bySettingHour: 22, minute: 0, second: 0, of: Date())!
-        while date <= endDate {
-            timesArray.append(formatter.string(from: date))
-            date = Calendar.current.date(byAdding: .minute, value: 15, to: date)!
-        }
-        return timesArray
-    }()
+    @State private var entryTimeIndex: Date = Calendar.current.date(bySettingHour: 8, minute: 0, second: 0, of: Date())!
+    @State private var exitTimeIndex: Date = Calendar.current.date(bySettingHour: 9, minute: 0, second: 0, of: Date())!
     
     var body: some View {
         VStack(spacing: 20) {
@@ -121,44 +148,32 @@ struct RoomReservationView: View {
                 .padding(.horizontal)
                 .overlay(
                     RoundedRectangle(cornerRadius: 7)
-                        .stroke(.blue, lineWidth: 2)
+                        .stroke(.blue, lineWidth: 1)
                         .padding(.horizontal)
                 )
-                
-            Picker("Horário de Entrada", selection: $entryTimeIndex) {
-                ForEach(0..<times.count, id: \.self) {
-                    Text(self.times[$0])
-                }
-            }
-            .pickerStyle(.menu)
-            .labelsHidden()
+            
+            DatePicker("Horário de Entrada",
+                       selection: $entryTimeIndex,
+                       in: Date()...,
+                       displayedComponents: .hourAndMinute
+            )
             .padding(.horizontal)
             .onChange(of: entryTimeIndex) { _, newValue in
                 adjustEntryTime(newValue)
             }
-            .overlay(
-                RoundedRectangle(cornerRadius: 7)
-                    .stroke(.blue, lineWidth: 2)
-                    .padding(.horizontal)
-            )
+            .datePickerStyle(.stepperField)
             
-            Picker("Horário de Saída", selection: $exitTimeIndex) {
-                ForEach(0..<times.count, id: \.self) {
-                    Text(self.times[$0])
-                }
-            }
-            .pickerStyle(.menu)
-            .labelsHidden()
+            DatePicker("Horário de Saída",
+                       selection: $exitTimeIndex,
+                       in: entryTimeIndex...,
+                       displayedComponents: .hourAndMinute
+            )
             .padding(.horizontal)
             .onChange(of: exitTimeIndex) { _, newValue in
                 adjustExitTime(newValue)
             }
-            .overlay(
-                RoundedRectangle(cornerRadius: 7)
-                    .stroke(.blue, lineWidth: 2)
-                    .padding(.horizontal)
-            )
-                                   
+            .datePickerStyle(.stepperField)
+            
             Spacer()
             
             Button(action: {
@@ -174,21 +189,29 @@ struct RoomReservationView: View {
         }
     }
     
-    func adjustEntryTime(_ newValue: Int) {
+    func adjustEntryTime(_ newValue: Date) {
         if newValue >= exitTimeIndex {
-            entryTimeIndex = exitTimeIndex - 1
+            entryTimeIndex = Calendar.current.date(bySettingHour: Calendar.current.component(.hour, from: exitTimeIndex),
+                                                   minute: Calendar.current.component(.minute, from: exitTimeIndex) - 15,
+                                                   second: 0,
+                                                   of: exitTimeIndex)!
         }
     }
     
-    func adjustExitTime(_ newValue: Int) {
+    func adjustExitTime(_ newValue: Date) {
         if newValue <= entryTimeIndex {
-            exitTimeIndex = entryTimeIndex + 1
+            exitTimeIndex = Calendar.current.date(bySettingHour: Calendar.current.component(.hour, from: entryTimeIndex),
+                                                  minute: Calendar.current.component(.minute, from: entryTimeIndex) + 15,
+                                                  second: 0,
+                                                  of: entryTimeIndex)!
         }
     }
     
     func reserveRoom() {
-        let entryTimeString = times[entryTimeIndex]
-        let exitTimeString = times[exitTimeIndex]
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        let entryTimeString = formatter.string(from: entryTimeIndex)
+        let exitTimeString = formatter.string(from: exitTimeIndex)
         print("Sala reservada por \(name) das \(entryTimeString) às \(exitTimeString)")
     }
 }
