c9c8665bc0f8ab72732584af611f874576f572cd
ricardosvale <ricardost3@gmail.com>
2024-08-09 15:00:31 -0300

    Feat: "Calendar Component"

--- PontoDesk/Features/Point/Components/ComponentsPointViewModel.swift
+++ PontoDesk/Features/Point/Components/ComponentsPointViewModel.swift
@@ -6,13 +6,94 @@
 //
 
 import SwiftUI
+import Combine
 
-struct ComponentsPointViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class ComponentsPointViewModel: ObservableObject {
+    @Published var date = Date(){
+        didSet{
+            updateDateInfo()
+            checkAdvanceButtonState()
+        }
     }
+    @Published  var dayOfWeek = ""
+    @Published  var formattedDate = ""
+    @Published  var dataInicial: Date?
+    @Published  var dataFinal: Date?
+    @Published  var totalHorasEMinutos = ""
+    @Published  var dataEntrada = ""
+    @Published  var horaEntrada = ""
+    @Published  var dataSaida = ""
+    @Published  var horaSaida = ""
+    @Published  var stateButton: (bool1: Bool, bool2: Bool, bool3: Bool) = (false, true, true)
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        updateDateInfo()
+        checkAdvanceButtonState()
+    }
+    
+    func updateTimeOnly() {
+        let calendar = Calendar.current
+        var components = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        let currentTime = Date()
+        components.hour = calendar.component(.hour, from: currentTime)
+        components.minute = calendar.component(.minute, from: currentTime)
+        components.second = calendar.component(.second, from: currentTime)
+        date = calendar.date(from: components) ?? date
+    }
+    
+    func updateDateInfo() {
+        dayOfWeek = getDayOfWeek(date: date)
+        formattedDate = formatDate(date: date)
+    }
+    
+    func checkAdvanceButtonState() {
+        let calendar = Calendar.current
+        let currentDate = calendar.startOfDay(for: Date())
+        let displayedDate = calendar.startOfDay(for: date)
+        
+        // Atualiza o estado do botão com base na comparação das datas
+        stateButton.bool3 = displayedDate == currentDate
+    }
+    
+    func getDayOfWeek(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "pt_BR")
+        dateFormatter.dateFormat = "EEEE"
+        return dateFormatter.string(from: date)
+    }
+    
+    func formatDate(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "pt-BR")
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .medium
+        return dateFormatter.string(from: date)
+    }
+    
+    func formatDatePonto(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "pt-BR")
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        return dateFormatter.string(from: date)
+    }
+    
+    func formatHourPonto(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "pt-BR")
+        dateFormatter.dateStyle = .none
+        dateFormatter.timeStyle = .short
+        return dateFormatter.string(from: date)
+    }
+    
+    func calculateTimeDifference(start: Date, end: Date) -> String {
+        let difference = Calendar.current.dateComponents([.hour, .minute], from: start, to: end)
+        let hours = difference.hour ?? 0
+        let minutes = difference.minute ?? 0
+        return "\(hours) horas e \(minutes) minutos"
+    }
+    
 }
 
-#Preview {
-    ComponentsPointViewModel()
-}
--- PontoDesk/Features/Point/Components/NavigationDateView.swift
+++ PontoDesk/Features/Point/Components/NavigationDateView.swift
@@ -8,11 +8,57 @@
 import SwiftUI
 
 struct NavigationDateView: View {
+    
+    @Binding var date: Date
+    var dayOfWeek: String
+    var formattedDate: String
+    var onPreviousDate: () -> Void
+    var onNextDate: () -> Void
+    var isAdvanceButtonEnabled: Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        HStack( alignment: .center)  {
+            Button(action: {
+                onPreviousDate()
+            }) {
+                Image(systemName: "chevron.left")
+                    .font(.system(size: 38, weight: .bold))
+                    .padding()
+                    .foregroundColor(.bgDarkBlue)
+            }
+           .buttonStyle(PlainButtonStyle())
+            
+            VStack (alignment: .center){
+               
+                Text(dayOfWeek.capitalized)
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.bgDarkBlue)
+                   
+                Text(formattedDate)
+                    .font(.title)
+                    .foregroundColor(.bgDarkBlue)
+                    .fontWeight(.semibold)
+                    .padding([.trailing, .leading], 10)
+                  
+            }
+         .frame(minWidth: 330, minHeight: 56)
+         
+            Button(action: {
+                onNextDate()
+            }) {
+                Image(systemName: "chevron.right")
+                    .padding()
+                    .font(.system(size: 38, weight: .bold))
+                    .foregroundColor(isAdvanceButtonEnabled ? Color.bgDarkBlue : Color.gray)
+                
+            }
+                .buttonStyle(PlainButtonStyle())
+                .disabled(!isAdvanceButtonEnabled)
+        }
+        .frame(minWidth: 365, minHeight: 56)
+        .padding()
     }
 }
 
-#Preview {
-    NavigationDateView()
-}
