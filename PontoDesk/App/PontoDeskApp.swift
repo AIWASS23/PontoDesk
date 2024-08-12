8133df45e9e1203e3f637c8c1d6f490f6c0efaab
Yasmin-Carloto <yasmincarlotobezerra17@gmail.com>
2024-08-12 15:23:19 -0300

    feat: adding behavior to onOpenURL to github button

--- PontoDesk/App/PontoDeskApp.swift
+++ PontoDesk/App/PontoDeskApp.swift
@@ -22,11 +22,19 @@ struct PontoDeskApp: App {
     var body: some Scene {
         WindowGroup {
             GeometryReader{reader in
+                
                 LoginView(screenSize: reader, currentUrl: $currentUrl)
                     .background(.bgScreen)
             }
+            .onOpenURL(perform: { url in
+                if let component =  URLComponents(url: url, resolvingAgainstBaseURL: true){
+                    print(component.queryItems?[0])
+                }
+            })
+            .handlesExternalEvents(preferring: ["pontodesk"], allowing: ["pontodesk"])
         
         }
+        
         Window("Entrar no PontoDesk", id: "auth"){
             LoginWebView(url: self.currentUrl)
                     .frame(minWidth: 900, maxWidth: 900, minHeight: 450, maxHeight: 450)
